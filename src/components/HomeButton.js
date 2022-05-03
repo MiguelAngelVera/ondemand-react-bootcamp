fb378d6bb796e8e6f208e7d0c025cf3a29a54e10
Miguel Vera Morales <miguel.vera@Miguels-MacBook-Pro.local>
2022-05-03 13:14:37 -0500

    eslint HomeButton

--- src/components/HomeButton.js
+++ src/components/HomeButton.js
@@ -5,10 +5,12 @@ import '../App.css'
 export default function HomeButton() {
   return (
     <Link
-        to="/ondemand-react-bootcamp/products"
-        style={{textDecoration: 'none'}}
-      >
-        <button className="App-button">View all Products</button>
-      </Link>
+      to="/ondemand-react-bootcamp/products"
+      style={{textDecoration: 'none'}}
+    >
+      <button type="button" className="App-button">
+        View all Products
+      </button>
+    </Link>
   )
 }
