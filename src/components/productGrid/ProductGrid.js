4f34b93c66f6a2133b8e52b255391b454aa1759e
Miguel Vera Morales <miguel.vera@Miguels-MacBook-Pro.local>
2022-05-03 12:46:18 -0500

    eslint productGrid

--- src/components/productGrid/ProductGrid.js
+++ src/components/productGrid/ProductGrid.js
@@ -1,33 +1,39 @@
-import {Link, useNavigate} from 'react-router-dom'
+import React, {Link} from 'react-router-dom'
+import PropTypes from 'prop-types'
 import AddToCart from '../cart/AddToCart'
 import * as styles from './ProductGrid-style'
 
-function Image(item) {
+function Image({
+  itemData,
+  itemImage,
+  itemName,
+  itemPrice,
+  itemCategory,
+  itemId,
+}) {
   return (
     <styles.ProductImage>
-        <img src={item.data.mainimage.url} alt={item.id} />
-        <styles.Line />
-        <styles.ProductText>
-          <styles.ProductPrice>${item.data.price}</styles.ProductPrice>
-          <styles.ProductName>
-            {item.data.name.length > 25
-              ? `${item.data.name.substring(0, 24)  }...`
-              : item.data.name}
-          </styles.ProductName>
-          <styles.ProductCategory>
-            {item.data.category.slug.toUpperCase().replace('--', ' & ')}
-          </styles.ProductCategory>
-          <styles.ProductDetails>
-            <Link
-              to={`/ondemand-react-bootcamp/products/${  item.id}`}
-              style={{textDecoration: 'none'}}
-            >
-              More Details
-            </Link>
-          </styles.ProductDetails>
-          <AddToCart data={item} qty={1} />
-        </styles.ProductText>
-      </styles.ProductImage>
+      <img src={itemImage} alt={itemName} />
+      <styles.Line />
+      <styles.ProductText>
+        <styles.ProductPrice>${itemPrice}</styles.ProductPrice>
+        <styles.ProductName>
+          {itemName.length > 25 ? `${itemName.substring(0, 24)}...` : itemName}
+        </styles.ProductName>
+        <styles.ProductCategory>
+          {itemCategory.toUpperCase().replace('--', ' & ')}
+        </styles.ProductCategory>
+        <styles.ProductDetails>
+          <Link
+            to={`/ondemand-react-bootcamp/products/${itemId}`}
+            style={{textDecoration: 'none'}}
+          >
+            More Details
+          </Link>
+        </styles.ProductDetails>
+        <AddToCart itemData={itemData} qty={1} />
+      </styles.ProductText>
+    </styles.ProductImage>
   )
 }
 
@@ -39,13 +45,36 @@ export default function ProductGrid({productisLoading, product, feature}) {
       ) : null}
       <styles.Container>
         {!productisLoading
-          ? product.map((item) => (
-              <styles.ProductCard key={item.data.sku + feature}>
-                <Image {...item} />
+          ? product.map((itemData) => (
+              <styles.ProductCard key={itemData.data.sku + feature}>
+                <Image
+                  itemData={itemData}
+                  itemImage={itemData.data.mainimage.url}
+                  itemName={itemData.data.name}
+                  itemPrice={itemData.data.price}
+                  itemCategory={itemData.data.category.slug}
+                  itemId={itemData.id}
+                />
               </styles.ProductCard>
             ))
           : null}
       </styles.Container>
     </>
   )
+}
+
+Image.propTypes = {
+  itemImage: PropTypes.string.isRequired,
+  itemId: PropTypes.string.isRequired,
+  itemName: PropTypes.string.isRequired,
+  itemPrice: PropTypes.number.isRequired,
+  itemCategory: PropTypes.string.isRequired,
+  itemData: PropTypes.oneOfType([PropTypes.string, PropTypes.object])
+    .isRequired,
+}
+
+ProductGrid.propTypes = {
+  productisLoading: PropTypes.bool.isRequired,
+  product: PropTypes.oneOfType([PropTypes.array]).isRequired,
+  feature: PropTypes.string.isRequired,
 }
